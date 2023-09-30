74815636ce369cd8364cb3a2eab7f71cf5f8ef58
Mauricio Maniely <mauriciomproduc@alu.ufc.br>
2023-09-29 23:07:33 -0300

    kkkk

--- autenticacao.js
+++ autenticacao.js
@@ -1,14 +1,18 @@
-function acessoProprietario() {
-    // Redirecione o usuário para a planilha com acesso total
-    window.location.href = "https://docs.google.com/spreadsheets/d/1T51HwazDE49zLuzqLeBH3egoXl_yb7tEI16VTKYga9k/edit#gid=1940797459";
+function acessoEntradasSaidas() {
+    // Redirecionar para o link de Entradas / Saídas
+    window.location.href = "https://docs.google.com/spreadsheets/d/1vEm83ZaHyXXXgpwoexTVqTpUbJuj7dVMqNOlP2tLbbg/edit#gid=0";
 }
 
-function acessoFuncionario() {
-    var senhaDigitada = prompt("Digite a senha de funcionário:");
-    if (senhaDigitada === "1234") {
-        // Redirecione o usuário para a planilha com acesso restrito
-        window.location.href = "https://docs.google.com/spreadsheets/d/1T51HwazDE49zLuzqLeBH3egoXl_yb7tEI16VTKYga9k/edit#gid=1940797459";
+function acessoAcessoRestrito() {
+    // Pedir senha ao usuário
+    var senha = prompt("Digite a senha para Acesso Restrito:");
+
+    // Verificar a senha
+    if (senha === "studiom") {
+        // Se a senha estiver correta, redirecionar para o link de Acesso Restrito
+        window.location.href = "https://docs.google.com/spreadsheets/d/1wxmhY-LPj9WbGDRK_3YQsBIeYE9YnbWkV0L3_UVF5N8/edit#gid=0";
     } else {
-        alert("Senha incorreta. Tente novamente.");
+        // Se a senha estiver incorreta, mostrar mensagem de erro
+        alert("Senha incorreta. Acesso negado.");
     }
 }
