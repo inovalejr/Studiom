111f34b581435f6ec008dae678d0b5b6a9585f7a
Studio M <146476340+inovalejr@users.noreply.github.com>
2024-02-02 20:51:01 -0300

    Update autenticacao.js

--- autenticacao.js
+++ autenticacao.js
@@ -8,7 +8,7 @@ function acessoAcessoRestrito() {
     var senha = prompt("Digite a senha para Acesso Restrito:");
 
     // Verificar a senha
-    if (senha === "studiom") {
+    if (senha === "15062003") {
         // Se a senha estiver correta, redirecionar para o link de Acesso Restrito
         window.location.href = "https://docs.google.com/spreadsheets/d/1T51HwazDE49zLuzqLeBH3egoXl_yb7tEI16VTKYga9k/edit#gid=0";
     } else {
